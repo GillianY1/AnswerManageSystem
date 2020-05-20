394e14735ee0a1511ea1edb7a9d4a8ef024ecf2c
GillianY1 <gilliangilliany@gmail.com>
2020-05-20 14:23:53 +0800

    first basic jquery base

---
+++ js/all.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    /*---header ham menu-----*/
+    $('.ham-icon').click(function(e){
+        e.preventDefault();
+        $('.header-menu').toggleClass('active');
+    });
+
+   
+});
